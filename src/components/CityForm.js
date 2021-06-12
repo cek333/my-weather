f2dd9c01d94fffcdd111480df87aa6c2949f3a89
Cecil <69444825+cek333@users.noreply.github.com>
2021-06-12 17:17:56 -0400

    implement form functionality

--- src/components/CityForm.js
+++ src/components/CityForm.js
@@ -1,15 +1,36 @@
-import React, { useState } from 'react';
-import { useSelector, useDispatch } from 'react-redux'
+import React, { useEffect, useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { refreshData } from '../app/store';
 
 function CityForm() {
+  const dispatch = useDispatch();
+
   const defaultCity = useSelector(state => state.weather.city);
-  const [city, setCity] = useState(defaultCity);
+  const [city, setCity] = useState('');
+
+  useEffect(() => {
+    setCity(defaultCity);
+  }, [defaultCity]);
+
+  function handleCityChange(evt) {
+    setCity(evt.target.value)
+  }
+
+  function handleSubmit(evt) {
+    evt.preventDefault();
+    if (city) {
+      dispatch(refreshData(city));
+    }
+  }
 
   return (
     <form>
       <label htmlFor='city'>Enter City</label>
-      <input type='text' id='city' placeholder='Please enter city.' value={city} />
-      <button className='muted-button' type='submit'>Refresh Weather</button>
+      <input type='text' id='city' name='city' placeholder='Please enter city.' value={city}
+        onChange={handleCityChange} required />
+      <button className='muted-button' type='submit' onClick={handleSubmit}>
+        Refresh Weather
+      </button>
     </form>
   );
 }
