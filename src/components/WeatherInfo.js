20e7391ff42cc3c371c081bf4ee38bddc173e7f2
Cecil <69444825+cek333@users.noreply.github.com>
2021-06-12 14:31:17 -0400

    ensure all paragraphs are displayed by using nbsp if necessary; use icon to indicate loading state; load weather on component load (i.e. useEffect())

--- src/components/WeatherInfo.js
+++ src/components/WeatherInfo.js
@@ -1,21 +1,38 @@
 import React, { useEffect } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
+import { refreshData } from '../app/store';
 
 function WeatherInfo() {
+  const dispatch = useDispatch();
+
   const weather = useSelector(state => state.weather);
   const timestamp = useSelector(state => state.timestamp);
   const city = weather.city ? weather.city : <>&nbsp;</>;
+  const description = weather.description ? weather.description : <>&nbsp;</>;
   const date = new Date(timestamp).toDateString();
-  const weatherIcon = `images/${weather.icon}_2x.png`;
   const status = useSelector(state => state.status);
   const error = useSelector(state => state.error) || <>&nbsp;</>;
+
+  // Re-use weather icon to show loading
+  let weatherIcon;
+  if (status === 'loading') {
+    weatherIcon = 'images/loading_2x.png';
+  } else {
+    weatherIcon = `images/${weather.icon}_2x.png`;
+  }
+
+  useEffect(() => {
+    if (status === 'idle') {
+      dispatch(refreshData('Toronto'));
+    }
+  }, []);
 
   return (
     <div className='text-center'>
       <h3>{city}</h3>
       <p>{date}</p>
       <p><img alt='weather icon' src={weatherIcon} /></p>
-      <p>{weather.description}</p>
+      <p>{description}</p>
       <p>Temperature: {weather.temperature}&#8451;</p>
       <p>Wind Speed: {weather.windSpeed}km/hr</p>
       <p className='has-error'>{error}</p>
